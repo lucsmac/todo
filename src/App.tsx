ec36083cc8e443c5bef72a4c89657087e44cbc8a
Lucas Macedo <lucasmdc57@gmail.com>
2023-02-27 17:15:11 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import todoImage from './assets/todo-logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={todoImage} alt="todo logotype" />
+    </div>
+  )
+}
+
+export default App
